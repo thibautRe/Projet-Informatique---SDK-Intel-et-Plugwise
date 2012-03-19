a250db3cc2483a73fe0a6c2a20ff6b6048e623a7
Thibaut REMY <thibaut.remy@telecom-sudparis.eu>
2012-03-19 02:40:40 +0100

    POL : Ajout d'un battery killer fait maison.

---
+++ programme/python/batteryKiller.py
@@ -0,0 +1,10 @@
+# -*- coding:utf-8 -*-
+
+import math
+
+a = 1
+b = 1
+while 1 :
+    a += math.sin(a)*math.cos(a + 1)
+    b -= math.sin(a*math.sin(math.tan(b*b*b*b)-math.cos(math.tan(math.cos(6854*a*b**a)))))
+    
